256cef5ea196d38233435f253c03435c562862b9
DMITRY <demofil@mail.ru>
2019-02-14 23:59:38 +0400

    debug

--- models/product.js
+++ models/product.js
@@ -71,7 +71,7 @@ module.exports = function(db, fx) {
 
     }
   };
-
+mongoose.plugin(schema => { schema.options.usePushEach = true });
   var schema = new mongoose.Schema(productSchema,{usePushEach : true });
   // var schema = new mongoose.Schema(productSchema, {usePushEach: true});
 
